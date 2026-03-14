d9223f50033b5cc16db1a0050fca9e7cbc379f4a
El Mehdi Oukid <el-mehdi.oukid.1@ens.etsmtl.ca>
2026-03-14 14:30:16 -0400

    Ajout d'un import

--- analyse.py
+++ analyse.py
@@ -1,4 +1,8 @@
+import argparse
+
 def main():
+    parser = argparse.ArgumentParser(description = "Script d'analyse de log")
+    parser.add_argument()
     print("Analyse des logs démarrée.")
 
 if __name__ == "__main__":
