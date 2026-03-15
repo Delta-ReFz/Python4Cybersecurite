e7dedd72414d1aaba4b992f4e2f332cf2e920ce2
El Mehdi Oukid <el-mehdi.oukid.1@ens.etsmtl.ca>
2026-03-15 00:55:03 -0400

    Ajout de modules

--- analyse.py
+++ analyse.py
@@ -2,8 +2,12 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description = "Script d'analyse de log")
-    parser.add_argument()
-    print("Analyse des logs démarrée.")
+    parser.add_argument(
+        "logpath" , help="Chemin vers les fichiers de logs à analyser", type = str
+    )
+    args = parser.parse_args()
+
+    print(f"Chemin des logs à analyser : {args.logpath} ")
 
 if __name__ == "__main__":
     main()
