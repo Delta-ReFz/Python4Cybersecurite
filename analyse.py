6bd7997ab4af74ece676bd16ff4d66093ee6f9d0
El Mehdi Oukid <el-mehdi.oukid.1@ens.etsmtl.ca>
2026-03-17 21:27:50 -0400

    Transfert vers cette branche

--- analyse.py
+++ analyse.py
@@ -1,11 +1,21 @@
 import argparse
+from modules.log_reader import LogReader
 
 def main():
     parser = argparse.ArgumentParser(description = "Script d'analyse de log")
     parser.add_argument(
-        "logpath" , help="Chemin vers les fichiers de logs à analyser", type = str
+        "repertoire" , help="Chemin vers le répertoire contenant les fichiers de logs à analyser", type = str
     )
     args = parser.parse_args()
+
+    lecteur = LogReader(args.repertoire)
+
+    fichiers_logs = lecteur.trouver_fichiers_logs()
+
+    if fichiers_logs:
+        for fichier_log in fichiers_logs:
+            print(f"\nLecture du fichier : {fichier_log}")
+            lecteur.lire_logs(fichier_log)
 
     print(f"Chemin des logs à analyser : {args.logpath} ")
 
--- modules/log_reader.py
+++ modules/log_reader.py
@@ -1,29 +1,43 @@
 import os
 
-def trouver_fichiers_logs(dossier, extension=".log"):
-    """
-    Parcourt un dossier et renvoie une liste de fichiers de logs avec l'extension spécifié
-    """
-    fichiers_logs = []
 
-    try: 
-        # Parcourt le dossier et récupère tous les fichiers avec l'extension donnée
-        for fichier in os.listdir(dossier):
-            if fichier.endswith(extension):
-                fichiers_logs.append(os.path.join(dossier,fichier))
-        return fichiers_logs
-    except FileNotFoundError:
-        print(f"Erreur : le dossier {dossier} n'a pas été trouvé.")
-        return[]
+class LogReader:
+    def __init__(self, repertoire):
+        """
+        Initialise un lecteur de logs avec une liste vide pour stocker les lignes lues.
+        """
+        self.repertoire = repertoire
+        self.lignes_lues = []
+
+    def trouver_fichiers_logs(self, extension=".log"):
     
-def lire_logs(fichier_log):
-    """
-    Lit un fichier de logs et renvoie son contenu sous forme de liste de lignes.
-    """
-    try:
-        with open(fichier_log, 'r') as f:
-            logs = f.readlines()
-        return logs
-    except FileNotFoundError:
-        print(f"Erreur : Le fichier {fichier_log} n'a pas été trouvé.")
-        return []
+        """
+        Parcourt le repertoire et renvoie une liste de fichiers de logs
+        avec l'extension spécifiée.
+        """
+        fichiers_logs = []
+
+        try:
+            # Parcourt le dossier et récupère tous les fichiers avec l'extension donnée
+            for fichier in os.listdir(self.repertoire):
+                if fichier.endswith(extension):
+                    fichiers_logs.append(os.path.join(self.repertoire, fichier))
+
+            return fichiers_logs
+
+        except FileNotFoundError:
+            print(f"Erreur : le repertoire {self.repertoire} n'a pas été trouvé.")
+            return []
+
+    def lire_logs(self, fichier_log):
+        """
+        Lit un fichier de logs et renvoie son contenu sous forme de liste de lignes.
+        """
+        try:
+            with open(fichier_log, "r") as f:
+                logs = f.readlines()
+            return logs
+
+        except FileNotFoundError:
+            print(f"Erreur : Le fichier {fichier_log} n'a pas été trouvé.")
+            return []
